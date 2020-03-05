23131672f3770cf32f36d78ab8736b8f34b33f67
Louis Ferraro <louis@navigatenorthstar.com>
2020-03-04 19:05:38 -0500

    adding travis ci

--- web/functions/index.js
+++ web/functions/index.js
@@ -1,6 +1,8 @@
 // This is where our endpoints go
 const functions = require("firebase-functions");
-const admin = require("firebase-admin");
+const admin = require("firebase-admin"); // the admin sdk
+const express = require("express");
+const app = express();
 
 // no need to pass any parameters beacause of .firebasesrc
 admin.initializeApp();
@@ -11,3 +13,6 @@ admin.initializeApp();
 exports.test = functions.https.onRequest((request, response) => {
   response.send("Testing...");
 });
+
+// ensure that our enpoints look like this no matter the route -> https://domain.com/api/
+exports.api = functions.https.onRequest(app);
