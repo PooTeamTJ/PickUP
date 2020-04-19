0912286a37ecc558393c5153a001f920cab81f46
Louis Ferraro <louis@navigatenorthstar.com>
2020-04-18 23:37:39 -0400

    passing token around components

--- mobile/Components/YourEvents.js
+++ mobile/Components/YourEvents.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import {
   View,
   Button,
@@ -6,36 +6,66 @@ import {
   Text,
   useColorScheme,
   FlatList,
+  AsyncStorage,
 } from "react-native";
 import EventComponent from "../Components/eventComponent";
 import { Card, ListItem, Icon } from "react-native-elements";
 // const { db } = require("../../web/functions/util/admin");
 
-// need to get events from users
-const events = [
-  { name: "Basketball at UCF", id: 1 },
-  { name: "soccer", id: 2 },
-  { name: "Lacrosse at the park", id: 3 },
-];
+export default class YourEvents extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: [
+        { name: "Basketball at UCF", id: 1 },
+        { name: "soccer", id: 2 },
+        { name: "Lacrosse at the park", id: 3 },
+      ],
+      token: "",
+    };
+  }
 
-const renderItem = ({ item }) => {
-  return (
-    <View>
-      <Text>
-        {item.name} id:{item.id}
-      </Text>
-    </View>
-  );
-};
+  // need to get events from users
 
-const YourEvents = () => {
-  return (
-    <React.Fragment>
-      <FlatList data={events} renderItem={renderItem} />
-    </React.Fragment>
-  );
-};
+  async getUserToken(key) {
+    try {
+      // need to wait to get token because render will load before lifecycle method
+      const retrievedItem = await AsyncStorage.getItem(key);
+      const token = JSON.parse(retrievedItem);
+      return token;
+    } catch (error) {
+      console.log("fail");
+    }
+  }
+
+  componentDidMount() {
+    this.getUserToken("token")
+      .then((value) => {
+        console.log(value);
+        // token is now in memory (state) and stored on the device for one hour
+        this.token = value;
+      })
+      .catch((error) => {
+        console.log("Error getting token");
+      });
+  }
+
+  renderItem = ({ item }) => {
+    return (
+      <View>
+        <Text>
+          {item.name} id:{item.id}
+        </Text>
+      </View>
+    );
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <FlatList data={this.state.events} renderItem={this.renderItem} />
+      </React.Fragment>
+    );
+  }
+}
 
 const styles = StyleSheet.create({});
-
-export default YourEvents;
