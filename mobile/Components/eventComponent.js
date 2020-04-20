dedc3747262bde3bcc79825a7ecb5e04b168a6d1
ItzikEfraim <itzikefraim6@gmail.com>
2020-04-20 12:42:55 -0400

    Map in eventComponent

--- mobile/Components/eventComponent.js
+++ mobile/Components/eventComponent.js
@@ -15,24 +15,34 @@ export default class EventComponent extends Component {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      rosterCountState: null,
+      createAtState: null,
+      descriptionState: null,
+      locationState: null,
+      maxPeopleState: null,
+      roseterCountState: null,
+      userIdState: null,
+      userImageState: null,
+      waitListState: null,
+      timeState: null,
+      err: null,
+      dateState: null,
+      sportState: null,
+      tokenState: ''
+    };
+
+    this.addPerson = this.addPerson.bind(this)
+    this.leaveEvent = this.leaveEvent.bind(this)
+
   }
 
-  state = {
-    rosterCountState: null,
-    createAtState: null,
-    descriptionState: null,
-    locationState: null,
-    maxPeopleState: null,
-    roseterCountState: null,
-    userIdState: null,
-    userImageState: null,
-    waitListState: null,
-    timeState: null,
-    err: null,
-    dateState: null,
-    sportState: null
-  };
 
+
+  //**####################################################################################################################**//
+  // get token
+  //**####################################################################################################################**//
   async getUserToken(key) {
     try {
       // need to wait to get token because render will load before lifecycle method
@@ -45,52 +55,119 @@ export default class EventComponent extends Component {
       console.log(error);
     }
   }
+  //**####################################################################################################################**//
+  // get token end
+  //**####################################################################################################################**//
 
 
-  addPerson = () => {
-    if (this.state.rosterCountState == this.maxPeopleState) {
-      this.state.err = "Event is full";
-    } else {
-      this.setState({ rosterCountState: this.state.rosterCountState + 1 });
-      this.state.err = null;
-    }
+
+  getCurrentRoster() {
+
+
+  }
+
+  //**####################################################################################################################**//
+  // join event
+  //**####################################################################################################################**//
+   addPerson  ()  {
+    // if (this.state.rosterCountState == this.maxPeopleState) {
+    //   this.state.err = "Event is full";
+    // } else {
+    //   this.setState({ rosterCountState: this.state.rosterCountState + 1 });
+    //   this.state.err = null;
+    // }
+
+    console.log('addPerson() ' + this.state.tokenState)
+
+    fetch(
+       `https://us-central1-pickup-proj.cloudfunctions.net/api/events/4NwUiP0WTtd7mrNWBFLR/tag`, //${this.props.eventID}/tag`, //GNLHb6HOvPl56tK2VEz2,
+      {
+        method: "GET",
+        headers: {
+          'Authorization': 'Bearer ' + this.state.tokenState
+
+        }
+      }
+    )
+    .then((response) => response.json())
+    .then((x) => {
+        console.log(x)
+        this.getEvent()
+
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
   };
 
+  //**####################################################################################################################**//
+  // join event end
+  //**####################################################################################################################**//
+
+
+  //**####################################################################################################################**//
+  // leave event
+  //**####################################################################################################################**//
   leaveEvent = () => {
     /** here we want to make sure the user is part of this event
         For now I'm checking that there are people in the event**/
-    if (this.state.rosterCountState == 0) {
-      this.state.err = "Event is Empty";
-    } else {
-      this.setState({ rosterCountState: this.state.rosterCountState - 1 });
-      this.state.err = null;
-    }
+    // if (this.state.rosterCountState == 0) {
+    //   this.state.err = "Event is Empty";
+    // } else {
+    //   this.setState({ rosterCountState: this.state.rosterCountState - 1 });
+    //   this.state.err = null;
+    // }
+
+    fetch(
+       `https://us-central1-pickup-proj.cloudfunctions.net/api/events/4NwUiP0WTtd7mrNWBFLR/untag`, //${this.props.eventID}/tag`, //GNLHb6HOvPl56tK2VEz2,
+      {
+        method: "GET",
+        headers: {
+          'Authorization': 'Bearer ' + this.state.tokenState
+
+        }
+      }
+    )
+    .then((response) => response.json())
+    .then((x) => {
+        console.log(x)
+        this.getEvent()
+
+      })
+      .catch((error) => console.error(error))
+      .finally(() => {
+        this.setState({ isLoading: false });
+      });
+
   };
+  //**####################################################################################################################**//
+  // leave event end
+  //**####################################################################################################################**//
 
 
-
-
+  //**####################################################################################################################**//
+  // get event
+  //**####################################################################################################################**//
   // here we get all the info from the data base by calling the route from backend
   async getEvent  ()  {
    // var item = await AsyncStorage.getItem('token')
    // var token = JSON.parse(item)
-   var token = await this.getUserToken('token')
+  //  var token = await this.getUserToken('token')
+  console.log('getEvent() ' + this.state.tokenState)
 
     fetch(
-       `https://us-central1-pickup-proj.cloudfunctions.net/api/events/${this.props.eventID}`, //GNLHb6HOvPl56tK2VEz2,
+       `https://us-central1-pickup-proj.cloudfunctions.net/api/events/4NwUiP0WTtd7mrNWBFLR`,//${this.props.eventID}`, //GNLHb6HOvPl56tK2VEz2,
       {
         method: "GET",
         headers: {
-          'Authorization': 'Bearer ' + token
+          'Authorization': 'Bearer ' + this.state.tokenState
 
           }
       }
     )
     .then((response) => response.json())
-
     .then((x) => {
-      console.log(x)
-
         this.setState({
           rosterCountState: x.rosterCount,
           locationState: x.location,
@@ -101,17 +178,33 @@ export default class EventComponent extends Component {
           sportState: x.sport
         });
       })
-
       .catch((error) => console.error(error))
-
       .finally(() => {
         this.setState({ isLoading: false });
       });
   };
+  //**####################################################################################################################**//
+  //      get event end
+  //**####################################################################################################################**//
 
+
+  //**####################################################################################################################**//
+  //  componentDidMount
+  //**####################################################################################################################**//
   componentDidMount() {
-    this.getEvent();
+    this.getUserToken("token")
+            .then((value) => {
+                this.setState({tokenState: value})
+
+            }).then(() => {
+              this.getEvent();
+            })
+            .catch((error) => {
+                console.log("Error getting token")
+            })
   }
+  //**####################################################################################################################**//
+  //**####################################################################################################################**//
 
   render() {
     return (
@@ -123,31 +216,30 @@ export default class EventComponent extends Component {
         <View style={styles.container}>
           {/** Here we get the event name as prop from the user**/}
           <View style={{ backgroundColor: "black", width: "100%" }}>
-            <Text style={styles.welcome}>{this.state.sport}</Text>
+            <Text style={styles.welcome}>{this.state.sportState}</Text>
           </View>
 
           {/** Here we get the event date, time, location, maxPeople as prop from the user**/}
           <View style={{ top: "2%", alignItems: "center" }}>
-            <Text style={{ fontSize: 60, color: "#FFF" }}>
+            <Text style={{ fontSize: 50, color: "#FFF" }}>
               {this.state.dateState}{" "}
             </Text>
-            <Text style={{ fontSize: 40, top: "5%", color: "#FFF" }}>
+            <Text style={{ fontSize: 30, top: "5%", color: "#FFF" }}>
               {this.state.timeState}{" "}
             </Text>
             <Text style={{ fontSize: 20, top: "10%", color: "#FFF" }}>
               {this.state.locationState}{" "}
             </Text>
-            <Text style={{ fontSize: 30, top: "20%", color: "#FFF" }}>
+            <Text style={{ fontSize: 20, top: "15%", color: "#FFF" }}>
               Participants: {this.state.rosterCountState} /  {this.state.maxPeopleState}{" "}
             </Text>
           </View>
 
           {/**place holder for a map**/}
           <View>
-            <Text style={{ fontSize: 70, top: "20%" }}>
-              {" "}
-              Place Holder for a map of the location
-            </Text>
+            <Image
+            style={styles.logo}
+            source={{uri:`https://maps.googleapis.com/maps/api/staticmap?center=${this.state.locationState}&zoom=13&size=600x300&maptype=roadmap&key=AIzaSyDajSMNySNArAGv-sLRldlp4lAKsZE-YnQ`}} />
           </View>
 
           {/** Buttons to join and leave event**/}
@@ -190,7 +282,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   welcome: {
-    fontSize: 70,
+    fontSize: 60,
     textAlign: "center",
     margin: 10,
     color: "#FFF",
@@ -209,7 +301,7 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     width: "90%",
-    top: "12%",
+    top: "20%",
     left: "2.5%",
   },
   backBtnContainer: {
@@ -219,4 +311,9 @@ const styles = StyleSheet.create({
     top: "-1%",
     left: "6%",
   },
+  logo: {
+    top:"20%",
+    width: 350,
+    height: 250,
+  }
 });
